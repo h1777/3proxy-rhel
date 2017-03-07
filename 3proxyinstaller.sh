33d0fa9c0e6650a8578e3a88d581a6888a4de9da
Helder <helderalmeida@Helders-MacBook-Pro.local>
2017-03-07 20:11:31 +0000

    still editing directories

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,8 +1,8 @@
 yum install epel-release && yum install 3proxy
 firewall-cmd --zone=public --add-port=3128/tcp --permanent
 firewall-cmd --reload
-mkdir /etc/3proxy/
-cd /etc/3proxy/
+cd /etc/
+rm -f 3proxy.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/.proxyauth
