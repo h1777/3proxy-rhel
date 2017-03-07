37a8b36672845dabfcee7673e1bf0a2e9858a683
Helder <helderalmeida@Helders-MacBook-Pro.local>
2017-03-07 19:13:41 +0000

    Edit sources

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -3,11 +3,8 @@ firewall-cmd --zone=public --add-port=1080/tcp --permanent
 firewall-cmd --reload
 mkdir /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/h1777/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/h1777/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
-cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/h1777/3proxy/master/3proxyinit
-chmod  +x /etc/init.d/3proxyinit
-update-rc.d 3proxyinit defaults
+systemctl enable 3proxy.service
