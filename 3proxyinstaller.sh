ad02763d5fc036ab3f9c78ed56c679af8388c8e9
Helder <helderalmeida@Helders-MacBook-Pro.local>
2017-03-08 15:09:01 +0000

    added systemctl command

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,6 +1,5 @@
 yum install epel-release && yum install 3proxy
 systemctl start firewalld
-systemctl enable firewalld.service
 firewall-cmd --zone=public --add-port=3128/tcp --permanent
 firewall-cmd --reload
 cd /etc/
@@ -9,3 +8,4 @@ chmod 600 /etc/3proxy.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/.proxyauth
 chmod 600 /etc/.proxyauth
 systemctl enable 3proxy.service
+systemctl enable firewalld.service
