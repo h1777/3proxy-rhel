0fcb22687dbbfa062389ecc133340c37e1b017b4
Helder <trentjnr@aol.com>
2017-03-11 10:51:05 +0000

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,11 +1,19 @@
-yum install epel-release && yum install 3proxy
+systemctl enable firewalld.service
 systemctl start firewalld
 firewall-cmd --zone=public --add-port=3128/tcp --permanent
 firewall-cmd --reload
-cd /etc/
-wget --no-check-certificate -N https://raw.githubusercontent.com/h1777/3proxy-rhel/master/3proxy.cfg
-chmod 600 /etc/3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/master/.proxyauth
-chmod 600 /etc/.proxyauth
-systemctl enable 3proxy.service
-systemctl enable firewalld.service
+wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.9.tar.gz
+tar xzf 0.8.9.tar.gz
+cd 3proxy-0.8.9
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/0.8.9/3proxy.cfg
+chmod 600 /etc/3proxy/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-rhel/0.8.9/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
+chmod +x /etc/3proxy/3proxy
+./3proxy 3proxy.cfg
+#systemctl enable 3proxy.service
